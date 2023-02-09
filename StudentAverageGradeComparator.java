9b1b22a943b55f642333dc0c0d761fbd22c526ea
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2023-02-09 10:46:56 +0300

    Update StudentAverageGradeComparator.java

--- StudentAverageGradeComparator.java
+++ StudentAverageGradeComparator.java
@@ -4,12 +4,15 @@ public class StudentAverageGradeComparator implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
         if(s1.getAverageGrade() > s2.getAverageGrade()){
-            return -1;
+            return -1; //Eğer ilk veri daha önce gelmeliyse -1 döndürürüz
         }else if(s2.getAverageGrade() > s1.getAverageGrade()){
-            return 1;
+            return 1; //İkinci veri daha önce gelmeliyse 1 döndürürüz
         } else{
-            return 0;
+            return 0; //Her iki veri de eşitse 0 döndürürüz
         }
     }
-
 }
+/*
+    Daha Kısa Bir Kod Yazmak İstersek Aşağıdaki Kod Satırı da İşimizi Çözecektir
+        return Double.compare(s2.getAverageGrade(), s1.getAverageGrade());
+ */
